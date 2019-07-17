214a9c2d0e14827b5e4cfbbaecfa069c4626cf5c
guoxiaozhuang4 <guoxiaozhuang4@outlook.com>
2019-07-18 00:45:46 +0800

    初始化项目

---
+++ README.md
@@ -0,0 +1,8 @@
+# 这是一个适用于公司点餐接龙的小程序，方便各位同学报名与统计~
+## 计划功能
+- 报名
+- 统计
+- 排序
+- PC端导出文档
+- more...  
+![](http://ww1.sinaimg.cn/large/c4b18d0dgy1g4c3p2plzoj20jp0e8mxg.jpg)
---
+++ miniprogram/app.ts
@@ -0,0 +1,11 @@
+//app.ts
+export interface IMyApp {
+  userInfoReadyCallback?(res: wx.UserInfo): void
+  globalData: {
+    userInfo?: wx.UserInfo
+  }
+}
+
+App<IMyApp>({
+  globalData: {}
+})
